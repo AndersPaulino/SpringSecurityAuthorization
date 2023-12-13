8d19243077374150133ac6f2b6b1c36f5ec5b6f2
Anderson Paulino <90426252+AndersPaulino@users.noreply.github.com>
2023-12-13 15:39:20 -0300

    Primeiro commit do projeto de Autorização com Spring Security

---
+++ app/src/test/java/br/com/spring/security/auth/app/AppApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.spring.security.auth.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
